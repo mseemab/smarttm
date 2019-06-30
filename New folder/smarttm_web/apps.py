de7279c479f3b8bcb8ba26000b2c3fb594eeb1c0
Seemab Hassan <mseemab@outlook.com>
2019-06-30 21:05:09 +0500

    minor fixes

--- New folder/smarttm_web/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SmarttmWebConfig(AppConfig):
-    name = 'smarttm_web'
--- New folder/smarttm_web/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from smarttm_web.models import User
-
-
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    class Meta():
-        model = User
-        fields = ('full_name','email','password')
